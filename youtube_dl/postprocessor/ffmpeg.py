37fc1985980a4f3f74bc57d33907127c4e44597d
Kevin Lopez <klopez@bu.edu>
2021-01-09 15:26:15 -0500

    trying printing out of ffmpeg process

--- youtube_dl/postprocessor/ffmpeg.py
+++ youtube_dl/postprocessor/ffmpeg.py
@@ -230,13 +230,13 @@ class FFmpegPostProcessor(PostProcessor):
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         stdout, stderr = p.communicate()
         
-#         while True:
-#            out = p.stderr.read(1)
-#            if out == '' and p.poll() != None:
-#              break
-#            if out != '':
-#              sys.stdout.write(out)
-#              sys.stdout.flush()
+        while True:
+            out = p.stderr.read(1)
+            if out == '' and p.poll() != None:
+                break
+            if out != '':
+                sys.stdout.write(out)
+                sys.stdout.flush()
         
         if p.returncode != 0:
             stderr = stderr.decode('utf-8', 'replace')
