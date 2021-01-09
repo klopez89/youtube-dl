78b96a9464fe49a0b3e566d82030be8c85b805e0
Kevin Lopez <klopez@bu.edu>
2021-01-09 15:40:14 -0500

    trying to get it working again

--- youtube_dl/postprocessor/ffmpeg.py
+++ youtube_dl/postprocessor/ffmpeg.py
@@ -231,15 +231,15 @@ class FFmpegPostProcessor(PostProcessor):
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         stdout, stderr = p.communicate()
         
-        child = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
+#         child = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
         
-        while True:
-            out = p.stderr.read(1)
-            if out == '' and p.poll() != None:
-                break
-            if out != '':
-                sys.stdout.write(out)
-                sys.stdout.flush()
+#         while True:
+#             out = p.stderr.read(1)
+#             if out == '' and p.poll() != None:
+#                 break
+#             if out != '':
+#                 sys.stdout.write(out)
+#                 sys.stdout.flush()
         
         if p.returncode != 0:
             stderr = stderr.decode('utf-8', 'replace')
